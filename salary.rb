9a6e689d549c39a90b9ada2e82107b684ea7ebb8
Federico Iachetti <iachetti.federico@gmail.com>
2013-05-20 11:08:12 -0300

    Added gitconfig and pair script

---
+++ salary.rb
@@ -0,0 +1,15 @@
+# 42
+
+def salary(hours_day, cost_hr)
+  hours_month = hours_day*5*4
+  hours_month * cost_hr
+end
+
+salary 6, 80                    # => 9600
+salary 8, 65                    # => 10400
+salary 8, 70                    # => 11200
+salary 8, 80                    # => 12800
+
+
+1/0.191121
+# => 5.2322873990822565
